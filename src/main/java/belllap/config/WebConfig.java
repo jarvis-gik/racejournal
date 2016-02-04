8b7e603346b9834ff0e47f5ca052a7a029bd0223
Adam LaPlante <adam.laplante@emc.com>
2016-02-03 17:15:59 -0700

    jackson json support

--- src/main/java/belllap/config/WebConfig.java
+++ src/main/java/belllap/config/WebConfig.java
@@ -1,8 +1,15 @@
 package belllap.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 /**
  * Created by alaplante on 2/3/16.
@@ -10,4 +17,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
+
 }
--- src/main/java/belllap/web/RaceController.java
+++ src/main/java/belllap/web/RaceController.java
@@ -3,27 +3,31 @@ package belllap.web;
 import belllap.domain.Race;
 import belllap.service.RaceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
  * Created by alaplante on 2/3/16.
  */
-@Controller
+@RestController
 public class RaceController {
 
     @Autowired
     RaceService raceService;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
-    public String home() {
+    @RequestMapping(value = "/race", method = RequestMethod.GET, produces = "application/json")
+    public Race race(@RequestParam("name") String name) {
         List<Race> races = raceService.loadRaceData();
-        return races.get(0).getName();
+        for(Race race : races) {
+            if(race.getName().toLowerCase().contains(name.toLowerCase())) return race;
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/races", method = RequestMethod.GET, produces = "application/json")
+    public List<Race> races() {
+        List<Race> races = raceService.loadRaceData();
+        return races;
     }
 }
