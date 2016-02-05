3119793fd2ca5345b30ebacf9840cbc991a3f016
Adam LaPlante <adam.laplante@emc.com>
2016-02-05 14:52:08 -0700

    pretty print enum

--- src/main/java/belllap/domain/RaceType.java
+++ src/main/java/belllap/domain/RaceType.java
@@ -1,12 +1,30 @@
 package belllap.domain;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * Created by alaplante on 2/2/16.
  */
 public enum RaceType {
-    CRITERIUM,
-    ROAD_RACE,
-    HILL_CLIMB,
-    TIME_TRIAL,
-    OTHER; 
+    CRITERIUM("Criterium"),
+    ROAD_RACE("Road Race"),
+    HILL_CLIMB("Hill Climb"),
+    TIME_TRIAL("Time Trial"),
+    OTHER("Other");
+
+    private String value;
+
+    RaceType(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
